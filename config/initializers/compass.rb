666cc06f512ba0a79f28e0cac0708650f5d220a1
Christian Höltje <docwhat@gerf.org>
2011-11-07 03:34:04 -0500

    Another attempt to fix heroku/compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,24 @@
-require 'fileutils'
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+# require 'fileutils'
+# FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+#
+# Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+#                                              :urls => ['/stylesheets'],
+#                                              :root => "#{Rails.root}/tmp")
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-    :urls => ['/stylesheets'],
-    :root => "#{Rails.root}/tmp")
+heroku = !!ENV['HEROKU_TYPE']
+css_dir = heroku ? 'tmp' : 'public'
+location = Rails.root + 'app/styles'
+
+unless Sass::Plugin.template_location_array.any? { |pair| pair.first.to_s == location.to_s }
+  Sass::Plugin.add_template_location(location, Rails.root + css_dir + 'stylesheets')
+end
+
+if heroku
+  Sass::Plugin.template_location_array.each do |template_location, css_location|
+    css_location.sub!(%r{/public/stylesheets$}, "/#{css_dir}/stylesheets")
+  end
+  Rails.configuration.middleware.insert_after('Sass::Plugin::Rack',
+                                              'Rack::Static',
+                                              :urls => ['/stylesheets'],
+                                              :root => "#{Rails.root}/tmp")
+end
