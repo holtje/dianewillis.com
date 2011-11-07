0755107f63681889adaa2bf20112f35ed2ed1bba
Christian Höltje <docwhat@gerf.org>
2011-11-06 22:56:36 -0500

    Added Google Analytics link

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -9,7 +9,7 @@ ActiveAdmin::Dashboards.build do
 
   section "Information tracking", priority: 10 do
     ul do
-      li "Link to google analytics"
+      li link_to "Google Analytics", "https://www.google.com/analytics/reporting/?id=40326979"
       li "Link to getsatisfaction"
     end
   end
