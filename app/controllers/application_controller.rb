c3060adb4225f8d089ac3c1e88f9a0ff8a210aaa
Christian Höltje <docwhat@gerf.org>
2011-12-08 13:13:27 -0500

    Force dianewillis.org to redirect to dianewillis.com
    This is to prevent SEO fragmentation and to unify the analytics.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'dianewillis.com'
+
   private
 
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def ensure_domain
+    return unless Rails.env.production?
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      redirect_to "http://#{APP_DOMAIN}#{request.path}", :status => :moved_permanently
     end
-    helper_method :current_user
+  end
 
-    def require_login
-      unless current_user
-        redirect_to login_url, :notice => "Please login first"
-      end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def require_login
+    unless current_user
+      redirect_to login_url, :notice => "Please login first"
     end
+  end
 end
