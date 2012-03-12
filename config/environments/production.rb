e186b94e42502ea33c0499776c0e088bee82eb67
Christian Höltje <docwhat@gerf.org>
2012-03-12 00:41:25 -0400

    Removed exception notifier is favor of logentries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,3 @@ Dianewillis::Application.configure do
   config.active_support.deprecation = :notify
 end
 
-# We want to be emailed on errors
-Dianewillis::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[DianeWillis] ",
-  :sender_address => %{"notifier" <notifier@dianewillis.com>},
-  :exception_recipients => %w{docwhat@gmail.com}
