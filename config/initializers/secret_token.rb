cc5616349952981ce73ec71ef943f49873a26b64
Christian Höltje <docwhat@gerf.org>
2013-01-10 23:07:24 -0500

    Load secret_token from ENV
    I shouldn't commit the secret_token into git. D'oh!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dianewillis::Application.config.secret_token = '1f9458216add18ce3ac22a49158367a59ea9d4d1a29e601bd1bfa8b811bd59c24635ce3e731b7f6d10b0800c35bb2fdb4f3159f7ba2981333a93f1faf9cb6966'
+Dianewillis::Application.config.secret_token = if Rails.env == 'production'
+                                                  ENV['RAILS_SECRET_TOKEN']
+                                                else
+                                                  '1f9458216add18ce3ac22a49158367a59ea9d4d1a29e601bd1bfa8b811bd59c24635ce3e731b7f6d10b0800c35bb2fdb4f3159f7ba2981333a93f1faf9cb6966'
+                                                end
+
