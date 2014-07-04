e8b8048e813815bfedfe962a88a4730ec24a2d7b
Christian Höltje <docwhat@gerf.org>
2014-07-04 15:54:12 -0400

    added new rspec config stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,10 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.include FactoryGirl::Syntax::Methods
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
